d3a401bcd016dc9dfc4cfbc48cd3696e01d0be7e
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-08-16 23:34:16 -0400

    Added page navigation

--- C#Simple/AppShell.xaml.cs
+++ C#Simple/AppShell.xaml.cs
@@ -5,6 +5,11 @@
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(C_Simple.HomePage), typeof(C_Simple.HomePage));
+            Routing.RegisterRoute(nameof(C_Simple.LoginPage), typeof(C_Simple.LoginPage));
+            Routing.RegisterRoute(nameof(C_Simple.InputsPage), typeof(C_Simple.InputsPage));
+            Routing.RegisterRoute(nameof(C_Simple.ContactPage), typeof(C_Simple.ContactPage));
+            Routing.RegisterRoute(nameof(C_Simple.AboutPage), typeof(C_Simple.AboutPage));
         }
     }
 }
