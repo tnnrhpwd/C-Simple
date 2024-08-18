74c9f30b3a511b0499a39e144ed05ab71b8ee0a4
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-08-17 22:31:06 -0400

    Implement fixed actions

--- src/CSimple/Pages/ActionPage.xaml.cs
+++ src/CSimple/Pages/ActionPage.xaml.cs
@@ -1,9 +1,87 @@
-namespace CSimple.Pages;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
-public partial class ActionPage : ContentPage
+namespace CSimple.Pages
 {
-    public ActionPage()
+    public partial class ActionPage : ContentPage
     {
-        InitializeComponent();
+        public ObservableCollection<KeyboardKeyAction> KeyboardKeys { get; set; }
+        public ObservableCollection<MouseAction> MouseOutputs { get; set; }
+        public ObservableCollection<WindowsCommand> WindowsCommands { get; set; }
+
+        public ICommand AddNewKeyCommand { get; set; }
+        public ICommand AddNewMouseActionCommand { get; set; }
+        public ICommand AddNewCommandCommand { get; set; }
+        public ICommand SimulateKeyCommand { get; set; }
+        public ICommand SimulateMouseCommand { get; set; }
+        public ICommand ExecuteCommand { get; set; }
+
+        public ActionPage()
+        {
+            InitializeComponent();
+
+            KeyboardKeys = new ObservableCollection<KeyboardKeyAction>();
+            MouseOutputs = new ObservableCollection<MouseAction>();
+            WindowsCommands = new ObservableCollection<WindowsCommand>();
+
+            AddNewKeyCommand = new Command(AddNewKey);
+            AddNewMouseActionCommand = new Command(AddNewMouseAction);
+            AddNewCommandCommand = new Command(AddNewCommand);
+
+            SimulateKeyCommand = new Command<KeyboardKeyAction>(SimulateKey);
+            SimulateMouseCommand = new Command<MouseAction>(SimulateMouse);
+            ExecuteCommand = new Command<WindowsCommand>(ExecuteWindowsCommand);
+
+            BindingContext = this;
+        }
+
+        private void AddNewKey()
+        {
+            KeyboardKeys.Add(new KeyboardKeyAction());
+        }
+
+        private void AddNewMouseAction()
+        {
+            MouseOutputs.Add(new MouseAction());
+        }
+
+        private void AddNewCommand()
+        {
+            WindowsCommands.Add(new WindowsCommand());
+        }
+
+        private void SimulateKey(KeyboardKeyAction action)
+        {
+            // Implement key simulation logic here.
+        }
+
+        private void SimulateMouse(MouseAction action)
+        {
+            // Implement mouse action simulation logic here.
+        }
+
+        private void ExecuteWindowsCommand(WindowsCommand command)
+        {
+            // Implement Windows command execution logic here.
+        }
+    }
+
+    public class KeyboardKeyAction
+    {
+        public string Key { get; set; }
+        public string ActionName { get; set; }
+    }
+
+    public class MouseAction
+    {
+        public string MouseAct { get; set; }
+        public string ActionName { get; set; }
+    }
+
+    public class WindowsCommand
+    {
+        public string Command { get; set; }
+        public string ActionName { get; set; }
     }
 }
